e00c7bb84ed1f844a5775b62ae199395b27a5387
AldoRamRod <aldogol1008@gmail.com>
2024-10-03 23:09:53 -0600

    RRA_ACT3.asm

--- RRA_ACT3.asm
+++ RRA_ACT3.asm
@@ -1,5 +1,5 @@
-;Programa que genera 20 n˙meros aleatorios y ordena de forma ascendente o descendente dependiendo de la elecciÛn del usuario
-;AUTHOR: RamÌrez RodrÌguez Aldo
+;Programa que genera 20 n√∫meros aleatorios y ordena de forma ascendente o descendente dependiendo de la elecci√≥n del usuario
+;AUTHOR: Ram√≠rez Rodr√≠guez Aldo
 
 ;MAIN PROGRAM
 
@@ -8,12 +8,12 @@
 
 INICIO:
     		LD A,89H		;SE CARGA EL VALOR EN A
-    		OUT (CW),A		;SE ENVÕA EL VALOR DE A AL PUERTO DE CONTROL CW
+    		OUT (CW),A		;SE ENV√çA EL VALOR DE A AL PUERTO DE CONTROL CW
     		LD HL,texto_entrada	;CARGA EL TEXTO DE ENTRADA EN EL REGISTRO HL
     		CALL mostrar_texto	;LLAMA LA SUBRUTINA MOSTRAR TETXO
     		LD HL,27FFH		
     		LD B,20
-    		CALL random		;LLAMA A LA SUBRUTINA QUE GENERA LOS 20 N⁄MEROS RANDOM
+    		CALL random		;LLAMA A LA SUBRUTINA QUE GENERA LOS 20 N√öMEROS RANDOM
     		LD HL,27FFH
     		LD B,20
     		CALL mostrar_num
@@ -24,7 +24,7 @@ INICIO:
     		JP Z,orden_ascen	;SI ES A, SALTA A LA SUBRUTINA DE ORDEN ASCENDENTE
     		CP 'D'			;SI NO ES A, COMPARA CON D
     		JP Z,orden_descen	;SI ES D, SALTA A LA SUBRUTINA DE ORDEN DESCENDENTE
-    		CALL mostrar_num_ord	;LLAMA A LA SUBRUTINA CON N⁄MEROS ORDENADOS
+    		CALL mostrar_num_ord	;LLAMA A LA SUBRUTINA CON N√öMEROS ORDENADOS
     		CALL pregunta		;LLAMA A LA SUBRUTINA DE LA PREGUNTA SOBRE SI DESEA CONTINUAR EN EL PROGRAMA
     		HALT
 
@@ -57,31 +57,30 @@ conv_decimal:
     		LD B,10
     		LD D,0
 
-decenas:
+num_decenas:
     		CP B
     		JP mostrar_unidades
     		SUB B
     		INC D
-    		JP decenas
+    		JP num_decenas
 
 mostrar_decenas:
     		LD A,D
     		ADD A,'0'
     		OUT (LCD),A
 
+num_unidades:
+    		CP B
+    		JP fin_conv
+    		SUB B
+    		INC D
+    		JP num_unidades
+
 mostrar_unidades:
     		LD A,(HL)
     		AND 0FH
     		LD B,10
     		LD D,0
-
-unidades:
-    		CP B
-    		JP fin_conv
-    		SUB B
-    		INC D
-    		JP unidades
-
 fin_conv:
     		LD A,D
     		ADD A,'0'
@@ -92,59 +91,59 @@ orden_ascen:
     		LD HL,27FFH
     		LD B,20
 
-ascendente_loop:
+asc_loop:
     		LD D,B
     		DEC D
     		LD HL,27FFH
 
-ascendente_inr_loop:
+asc_int:
     		LD A,(HL)
     		LD C,(HL+1)   ;Carga el siguiente valor
     		CP C
-    		JR C,nsw_ascendente
-    		LD (HL),C     ;Intercambia los valores si no est·n en orden
+    		JR C,nsw_asc
+    		LD (HL),C     ;Intercambia los valores si no est√°n en orden
     		LD (HL+1),A
 
-nsw_ascendente:
+nsw_asc:
     		INC HL
-    		INC HL        ;Avanza al siguiente par de n˙meros
-    		DJNZ ascendente_inr_loop
-   		DJNZ ascendente_loop
+    		INC HL        ;Avanza al siguiente par de n√∫meros
+    		DJNZ asc_int
+   		    DJNZ asc_loop
     		RET
 
 orden_descen:
     		LD HL,27FFH
     		LD B,20
 
-descendente_loop:
+desc_loop:
     		LD D,B
     		DEC D
     		LD HL,27FFH
 
-descendente_inr_loop:
+desc_int:
     		LD A,(HL)
     		LD C,(HL+1)   ;Carga el siguiente valor
     		CP C
-    		JR NC,nsw_descendente
-    		LD (HL),C     ;Intercambia los valores si no est·n en orden
+    		JR NC,nsw_desc
+    		LD (HL),C     ;Intercambia los valores si no est√°n en orden
     		LD (HL+1),A
 
 nsw_descendente:
     		INC HL
-    		INC HL        ;Avanza al siguiente par de n˙meros
-    		DJNZ descendente_inr_loop
-    		DJNZ descendente_loop
+    		INC HL        ;Avanza al siguiente par de n√∫meros
+    		DJNZ desc_int
+    		DJNZ desc_loop
     		RET
 
 mostrar_num_ord:
     		LD HL,27FFH
     		LD B,20
 
-loop_mstr_ord:
+loop_mostrar_ord:
     		LD A,(HL)
     		CALL conv_decimal
     		INC HL
-    		DJNZ loop_mstr_ord
+    		DJNZ loop_mostrar_ord
     		RET
 
 pregunta:
@@ -158,7 +157,7 @@ pregunta:
 
 fin_programa:
     		LD HL,texto_final
-   		CALL mostrar_texto
+   		    CALL mostrar_texto
     		HALT
 
 ;DATA SEGMENT
@@ -167,9 +166,9 @@ fin_programa:
 LCD  		.equ 01h
 KEYB	 	.equ 02h
 CW     		.equ 03h
-texto_entrada 	.db "INICIANDO PROGRAMA, CARGANDO N⁄MEROS ALEATORIOS&"
-orden_texto 	.db "øORDENAR ASCENDENTE O DESCENDENTE?&"
-pregunta_texto 	.db "øREPETIR (R) O SALIR (S)?&"
+texto_entrada 	.db "INICIANDO PROGRAMA, CARGANDO N√öMEROS ALEATORIOS&"
+orden_texto 	.db "¬øORDENAR ASCENDENTE O DESCENDENTE?&"
+pregunta_texto 	.db "¬øREPETIR (R) O SALIR (S)?&"
 texto_final	.db "HA SALIDO DEL PROGRAMA.&"
 
 ;END OF PROGRAM
